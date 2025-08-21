3476646aeb6d35cfc613fe111581b9eaf308704b
Ahmed Azzam <ahmedazzam.business@gmail.com>
2025-08-22 01:25:14 +0300

    fixed some typos

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -33,7 +33,7 @@ output "aks_cluster_name" {
   description = "AKS cluster name"
 }
 
-output "resource_group" {
+output "resource_group_name" {
   value       = azurerm_resource_group.rg.name
   description = "Resource group name"
 }
